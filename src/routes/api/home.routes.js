48396c8b785b95a894ccc1fbcc43ac06f8462a9b
Buddy-Alan <103333420+Buddy-Alan@users.noreply.github.com>
2023-03-24 20:50:31 -0300

    Update home.routes.js

--- src/routes/api/home.routes.js
+++ src/routes/api/home.routes.js
@@ -3,8 +3,10 @@ const home = Router()
 
 
 home.get("/", (req, res) => {
-
-    res.json({ "Message:": "Bienvenido a la simulacion del ecommerce" })
+    res.json({
+        "Message:": "Bienvenido a la simulacion del ecommerce , para conocer  un poco mas de como funciona acceda al siguiente enlace",
+        "Link": "https://github.com/Buddy-Alan/Carrito-Ecommerce-Express/#readme"
+    })
 })
 
 export default home
